1a25ca7272841cf87bad99e889f17957282242e9
eduardobentorres <eduardobentorres@gmail.com>
2020-11-14 11:53:57 -0600

    Added comments and updated Read.Me file

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
+//Using moment.js to grab and append current date to id "currentDay"
 var date = moment().format("MMM Do YY");
 
 $("#currentDay").append(date);
 
 console.log(date);
 
+//Creating currentTime variable to be in 24 hour format
 var currentTime = moment().format("k");
 console.log(currentTime);
 
+
+//Creating a loop that color coordinates hours based on their Id's
 for ( i =9; i<=17; i++){
     if (i<currentTime){
     $("#" + i + "todo").addClass("past")
@@ -21,11 +25,13 @@ for ( i =9; i<=17; i++){
    
 }
 
-
+//Attempt at creating a save function which stores users input into Local Storage and displays it 
 $(".saveBtn").on("click",function(){
-    var time = $(this).attr("#i");
+    var time = $(this).attr("id");
     var text = $("#" + time + "todo").val();
-    console.log(text,time);
+    console.log(time,text);
+    localStorage.getItem(time,text);
+    
 
 
 
